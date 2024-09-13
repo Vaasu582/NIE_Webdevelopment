d16cba8bb2392941042408dc5951618b2cb72e06
Vaasu582 <2023cs_srinivasac_d@nie.ac.in>
2024-09-13 14:50:47 +0530

    Add files via upload

---
+++ NIE/zay/views.py
@@ -0,0 +1,13 @@
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def shop(request):
+    return render(request, 'shop.html')
+def contact(request):
+    return render(request, 'contact.html')
